da2821362101747b5f3a6c031a6cd2a62b12616a
ilijaleko <ilija.leko.86@gmail.com>
2024-10-19 10:48:17 +0200

    Add API validators

---
+++ Hackathon.Service/ApiValidators/SupervisorCreateRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Hackathon.Service.ApiRequests;
+
+namespace Hackathon.Service.ApiValidators;
+
+public class SupervisorCreateRequestValidator : AbstractValidator<SupervisorCreateRequest>
+{
+    public SupervisorCreateRequestValidator()
+    {
+        // TODO: Add email validator
+        RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required");
+        RuleFor(x => x.Username).NotEmpty().WithMessage("Username is required");
+        // TODO: Add password validator
+        RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required");
+        RuleFor(x => x.FirstName).NotEmpty().WithMessage("First name is required");
+        RuleFor(x => x.LastName).NotEmpty().WithMessage("Last name is required");
+        // TODO: Add tenant ID validator
+        RuleFor(x => x.TenantId).NotEmpty().WithMessage("Tenant ID is required");
+    }
+}
---
+++ Hackathon.Service/ApiValidators/TenantCreateRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Hackathon.Service.ApiRequests;
+
+namespace Hackathon.Service.ApiValidators;
+
+public class TenantCreateRequestValidator : AbstractValidator<TenantCreateRequest>
+{
+    public TenantCreateRequestValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required");
+    }
+}
