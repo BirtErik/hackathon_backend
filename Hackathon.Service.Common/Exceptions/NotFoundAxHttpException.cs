e7614dd25dc25ec03bdeadef792f5decf5f80c29
ilijaleko <ilija.leko.86@gmail.com>
2024-10-19 10:49:20 +0200

    Add not found exception and db configuration

---
+++ Hackathon.Service.Common/Exceptions/NotFoundAxHttpException.cs
@@ -0,0 +1,8 @@
+﻿namespace Hackathon.Service.Common.Exceptions;
+
+public class NotFoundAxHttpException: BaseAxHttpException
+{
+    public NotFoundAxHttpException(string errorCode, Exception? outerException = null, string? errorMessage = ApiErrorCodes.CONTENT_RESOURCE_NOT_FOUND, string? moreInfo = null)
+
+        : base(errorCode, 404, outerException, errorMessage, moreInfo) { }
+}
