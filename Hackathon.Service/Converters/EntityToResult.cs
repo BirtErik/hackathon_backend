14cc033b2dabdab16eef6a3ff70965f71bd052c9
ilijaleko <ilija.leko.86@gmail.com>
2024-10-19 10:50:02 +0200

    Add converter

---
+++ Hackathon.Service/Converters/EntityToResult.cs
@@ -0,0 +1,28 @@
+﻿using Hackathon.Service.ApiRequests.Models;
+using Hackathon.Service.ApiResults;
+using Hackathon.Service.DAL.Entities;
+
+namespace Hackathon.Service.Converters;
+
+public static class EntityToResult
+{
+    public static VenueResult ToResult(this VenueEntity venueEntity)
+    {
+        return new VenueResult
+        {
+            Id = venueEntity.Id,
+            Name = venueEntity.Name,
+            Description = venueEntity.Description,
+            IsRentable = venueEntity.IsRentable,
+            Location = venueEntity.Location,
+            Capacity = venueEntity.Rooms.Sum(x => x.Capacity),
+            Rooms = venueEntity.Rooms.Select(x => new VenueRoom
+            {
+                Name = x.Name,
+                Description = x.Description,
+                Capacity = x.Capacity,
+                IsRentable = x.IsRentable
+            }).ToList()
+        };
+    }
+}
